da902df8ff70c9c0e352a2a1331e1be11183ac3c
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-02-24 17:13:50 +0100

    Added file footer.php

---
+++ src/footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+    <!-- segue documento specifico php e chiude il file Header.php-->
+    <br>Copyright &commat; 2019 - All Right Reserved - 1280 x 1024 O.R.
+</div>
+
+</body>
+
+</html> <!-- Chiude Documento Html aperto in Header.php-->
