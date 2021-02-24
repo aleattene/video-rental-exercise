5cfcbac7a390833e4c4f4046f3d11491a5d3860c
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-02-24 17:09:46 +0100

    Added file calcoloincassi.php

---
+++ src/calcoloincassi.php
@@ -0,0 +1,22 @@
+<?php
+include "sessioncontrol.php";
+include "header.php";
+include "body.php";
+?>
+<div id="project-section">
+    <div id=subproject1>
+        <?php
+        echo "<input type=\"button\" 
+            onclick=\"location.href='incassooperatore.php'\" value=\"Incasso Operatore\"/>";
+        ?>
+    </div>
+    <div id=subproject2>
+        <?php
+        echo "<input type=\"button\" 
+            onclick=\"location.href='incassopuntovendita.php'\" value=\"Incasso Punto Vendita\"/>";
+        ?>
+    </div>
+</div>
+</div> <!-- riguarda la chiusura di CONTAINER che si trova sul file BODY.php -->
+
+<?php include "footer.php"; ?>
