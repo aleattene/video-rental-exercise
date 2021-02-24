ec7b053ede5f56fda6d36c5a76a81ad46593269c
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-02-24 17:14:11 +0100

    Added file header.php

---
+++ src/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> <!-- Inizio documento Html in Header.php e Fine in Footer.php -->
+
+<html lang="it">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8" />
+
+    <title> BDC project </title>
+
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+
+</head>
+
+<!-- Inizio sezione BODY in Header.php e Fine in Footer.php -->
+
+<body>
+
+    <div id="wrapper">
+
+        <div id="header">
+
+            <div id="logo">
+                <img src="../assets/img/unigm_logo.jpg" alt="" width="240px" height="126px">
+            </div>
+
+            <div id="intestazione">
+                <h1>Università degli Studi <br> -------- </h1>
+            </div>
+
+            <div id="realizzazione">
+                <h3>Corso di Laurea in Ingegneria Informatica<br>
+                    Insegnamento: -------- <br>
+                    Docente: Prof. -------- <br>
+                    Realizzazione: -------- <br>
+                    Matricola: --------
+                </h3>
+            </div>
+
+        </div> <!-- segue nello specifico file php e si conclude con Footer.php -->
